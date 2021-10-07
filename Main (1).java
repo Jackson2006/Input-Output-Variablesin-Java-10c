af690b324dddbbc31bafb32be58ff4c04f866a50
Jackson2006 <92122210+Jackson2006@users.noreply.github.com>
2021-10-07 15:43:45 -0600

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println ("Sup homies");
+  }
+}
+/*This is a comment*/
+//This is another way to comment
+/*Java Variable types
+You have 5 TYPES OF VAIABLES IN Java 
+Boolean: True/False, only 2 state - boolean
+Integers: Int , always WHOLE numbers
+Strings: tEXT , ex "Hello" 
+FLoats: Decimals , ex 19.99 or -19.99
+Char: Single characters, "a", "B", WRITE A CODE THAT GIVES YOU HOW MANY CHARACTERS ARE IN "Pneumonoultramicroscopicsilicovolcanoconiosis"
+
+
+In math x = 4 means 4 = x
+In programming x = 4 (4 is assigned to x)
+
+x = "Patty"
+b = "Tomato"
+c = "Cheese"
+d = "Bread"
+e = "Mayo"
+f = "Ketchup"
+g = "Bacon"
+type variable = value;
+*/ 
